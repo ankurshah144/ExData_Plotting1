5a5adb064424957c7d9fd7f7d84151d18b7172f7
ankurshah144 <ankur_shah144@yahoo.co.in>
2014-08-04 21:35:30 -0700

    Added plot2 and plot3
    legend in output of plot3 is getting cut-off

---
+++ plot2.R
@@ -0,0 +1,28 @@
+## R script to plot a line graph for Exploratory Data Analysis
+## Course Project 1, Plot 2
+
+library(data.table)
+
+## Set working direcotry
+setwd("~/DS Toolbox/Exploratory Data Analysis")
+## Read large input file
+data <- fread("household_power_consumption.txt", sep=";", na.strings="?"
+                , sel=c("Date", "Time", "Global_active_power"))
+
+## Create subset of data to use
+datasub <- subset(data, data$Date %in% c("1/2/2007", "2/2/2007"))
+
+## Convert columns for use in plotting
+datasub$Date <- as.Date(datasub$Date, "%d/%m/%Y")
+datasub$Global_active_power <- as.numeric(datasub$Global_active_power)
+
+## Create plot to match course project requirements
+with(datasub
+        , plot(strptime(paste(datasub$Date, datasub$Time), "%Y-%m-%d %H:%M:%S")
+        , Global_active_power, type="l"
+        , xlab="", ylab="Global Active Power (kilowatts)"))
+
+setwd("~/DS Toolbox/Github/ExData_Plotting1")
+## Copy to .png file
+dev.copy(png, file="plot2.png", width=480, height=480, units="px")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,38 @@
+## R script to generate a comparison plot for Exploratory Data Analysis
+## Course Project 1, Plot 3
+
+library(data.table)
+
+## Set working direcotry
+setwd("~/DS Toolbox/Exploratory Data Analysis")
+## Read large input file
+data <- fread("household_power_consumption.txt", sep=";", na.strings="?"
+                , sel=c("Date", "Time", "Sub_metering_1"
+                            , "Sub_metering_2", "Sub_metering_3"))
+
+## Create subset of data to use
+datasub <- subset(data, data$Date %in% c("1/2/2007", "2/2/2007"))
+
+## Convert columns for use in plotting
+datasub$Date <- as.Date(datasub$Date, "%d/%m/%Y")
+datasub$Sub_metering_1 <- as.numeric(datasub$Sub_metering_1)
+datasub$Sub_metering_2 <- as.numeric(datasub$Sub_metering_2)
+
+## Date/Time to use in plotting
+x <- strptime(paste(datasub$Date, datasub$Time), "%Y-%m-%d %H:%M:%S")
+
+## Create plot to match course project requirements
+with(datasub, plot(x, Sub_metering_1, type="n"
+                    , xlab="", ylab="Energy sub metering"))  ## initialize
+with(subset(datasub), points(x, Sub_metering_1, type="l", col="black"))
+with(subset(datasub), points(x, Sub_metering_2, type="l", col="red"))
+with(subset(datasub), points(x, Sub_metering_3, type="l", col="blue"))
+## set legend; took lots of trial & errors and Stack Overflow reads!
+legend("topright", pch=NA, lwd=1, lty=1
+        , col=c("black", "red", "blue")
+        , legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+setwd("~/DS Toolbox/Github/ExData_Plotting1")
+## Copy to .png file
+dev.copy(png, file="plot3.png", width=480, height=480, units="px")
+dev.off()
