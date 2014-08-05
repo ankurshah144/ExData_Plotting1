9bff50a3f31cebeed8e0e2d5be33f07310d74015
ankurshah144 <ankur_shah144@yahoo.co.in>
2014-08-04 19:17:06 -0700

    plot1 created successfully

--- plot1.R
+++ plot1.R
@@ -1,25 +1,21 @@
 ## R script to plot a histogram for Exploratory Data Analysis
 ## Course Project 1, Plot 1
 
+library(data.table)
+
 ## Set working direcotry
 setwd("~/DS Toolbox/Exploratory Data Analysis")
-## Read large input file
-data <- fread("household_power_consumption.txt", sep=";", na.strings="?"
-                , colClasses=c("list", "character", "numeric", "numeric"
-                                , "numeric", "numeric", "numeric", "numeric"
-                                , "numeric"))
+## Read only the required columns from large input file
+data <- fread("household_power_consumption.txt", sep=";"
+                , na.strings="?", select=c("Date", "Global_active_power"))
 
 ## Create subset of data to use
 datasub <- subset(data, data$Date %in% c("1/2/2007", "2/2/2007"))
 
 ## Convert columns for use in plotting
 datasub$Date <- as.Date(datasub$Date, "%d/%m/%Y")
+## fread didn't convert "?" to NA even though I used na.strings
 datasub$Global_active_power <- as.numeric(datasub$Global_active_power)
-datasub$Global_reactive_power <- as.numeric(datasub$Global_reactive_power)
-datasub$Voltage <- as.numeric(datasub$Voltage)
-datasub$Global_intensity <- as.numeric(datasub$Global_intensity)
-datasub$Sub_metering_1 <- as.numeric(datasub$Sub_metering_1)
-datasub$Sub_metering_2 <- as.numeric(datasub$Sub_metering_2)
 
 ## Create histogram to match course project requirements
 hist(datasub$Global_active_power, col="red"
